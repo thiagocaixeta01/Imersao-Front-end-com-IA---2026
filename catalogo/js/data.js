491ba5a9c95de9ffd8001247528ee3842d6b10f9
thiagocaixeta01 <thiagocaixetatavares@gmail.com>
2026-03-27 16:10:25 -0400

    changes

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -23,19 +23,35 @@ export const categories = [
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { 
+                img: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/78e15665-f6c8-4b58-8786-e0d9e2b65ced/09341514ec38f76a8db253cd43e5791c6096ba46.jpg?host=wbd-images.prod-vod.h264.io&partner=beamcom&w=500", top10: true, badge: "Nova temporada", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=tNcDHWpselE" },
+            { 
+                img: "https://m.media-amazon.com/images/S/pv-target-images/cc417aabd67d431ba6d6dc5571f540a1a65acdf833458242925d8193ba4e6b2b.jpg", top10: true, 
+                youtube: "https://www.youtube.com/watch?v=MdXHLVr2T08&t=1s" },
+            { 
+                img: "https://blog.optemais.com.br/wp-content/uploads/2022/06/Capa.jpg", badge: "Novo episódio", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=RMmGQNNl164&t=2s" },
+            { 
+                img: "https://i0.wp.com/3.bp.blogspot.com/--gs7qxs2SQA/WqMplx5w5ZI/AAAAAAAAQ60/dLZ4wwonGe8YBCdSKkB1Opys4jD3jmz9gCLcBGAs/s1600/YoungSheldon-2000.jpg?ssl=1", badge: "Novidade", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=9AVt5oyzMbw" },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { 
+                img: "https://ovicio.com.br/wp-content/uploads/2022/12/20221223-ovicio-smallville-capa.jpg", top10: true, 
+                youtube: "https://www.youtube.com/watch?v=70Y32si4yb8&t=4s" },
+            { 
+                img: "https://m.media-amazon.com/images/S/pv-target-images/b71b06873fb90830776f41793940dddf06e5f38de22aa19e456d1aa3e1da483f.jpg", top10: true, badge: "Novidade", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=HKVve_VSz58" },
+            { 
+                img: "https://i.pinimg.com/736x/9d/47/58/9d4758720ddee877c665ce830f17d077.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=IgVyroQjZbE" },
+            { 
+                img: "https://claquetes.wordpress.com/wp-content/uploads/2015/07/suits.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", 
+                youtube: "https://www.youtube.com/watch?v=cUnkjEIW2-o" },
         ]
     }
 ];
